2850cdc55b87c278a0cba0636ebd777fc7858282
lixiang <lixiang@126.com>
2018-12-21 15:03:16 +0800

    #init

---
+++ .umirc.js
@@ -0,0 +1,23 @@
+
+// ref: https://umijs.org/config/
+export default {
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    ['umi-plugin-react', {
+      antd: true,
+      dva: true,
+      dynamicImport: true,
+      title: 'umi',
+      dll: true,
+      routes: {
+        exclude: [],
+      },
+      hardSource: true,
+      routes: {
+        exclude: [
+          /components/,
+        ],
+      },
+    }],
+  ],
+}
